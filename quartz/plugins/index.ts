cc2a158a261624f36c2cc53ab8da852967fb57cd
MasterNoiza <masternoiza@proton.me>
2025-12-31 23:10:37 +0100

    Update index.ts

--- quartz/plugins/index.ts
+++ quartz/plugins/index.ts
@@ -45,6 +45,7 @@ export function getStaticResourcesFromPlugins(ctx: BuildCtx) {
 export * from "./transformers"
 export * from "./filters"
 export * from "./emitters"
+export { LeafletMap } from "./leafletMapPlugin"
 
 declare module "vfile" {
   // inserted in processors.ts
